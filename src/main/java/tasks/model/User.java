ca657a55be87e4b50bc3431cc944741933122a55
JDY(HOME) <wlsejrdyd@gmail.com>
2025-04-29 17:35:38 +0900

    회원가입 성공

--- src/main/java/tasks/model/User.java
+++ src/main/java/tasks/model/User.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/tasks/service/UserService.java
+++ src/main/java/tasks/service/UserService.java
@@ -1,20 +1,28 @@
 package tasks.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import tasks.model.User;
 import tasks.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void registerUser(User user) {
+        logger.info("회원가입 시도: {}", user);
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
+
+        User saved = userRepository.save(user);
+        logger.info("저장된 사용자 ID: {}", saved.getId());
     }
 }
