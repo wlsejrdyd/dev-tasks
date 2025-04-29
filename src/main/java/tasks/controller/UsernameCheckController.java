0410fe85f2a909bd872f26fcdee68a32ba28a24f
JDY(HOME) <wlsejrdyd@gmail.com>
2025-04-29 21:59:56 +0900

    29일 마지막 커밋

---
+++ src/main/java/tasks/controller/UsernameCheckController.java
@@ -0,0 +1,18 @@
+package tasks.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tasks.repository.UserRepository;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class UsernameCheckController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/check-username")
+    public boolean checkUsername(@RequestParam String username) {
+        return !userRepository.existsByUsername(username);
+    }
+}
