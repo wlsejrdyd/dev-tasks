427c45b1c9d4cb77f868b1837d73eb88e4c3f26e
JDY(SERVER) <wlsejrdyd@gmail.com>
2025-05-15 23:45:30 +0900

    상세 IP 카드 수정

--- src/main/java/tasks/service/IpAddressService.java
+++ src/main/java/tasks/service/IpAddressService.java
@@ -1,11 +1,16 @@
 package tasks.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tasks.entity.IpAddress;
+import tasks.entity.User;
 import tasks.repository.IpAddressRepository;
+import tasks.repository.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,7 @@ import java.util.Optional;
 public class IpAddressService {
 
     private final IpAddressRepository ipAddressRepository;
+    private final UserRepository userRepository;
 
     public List<IpAddress> findAll() {
         return ipAddressRepository.findAll();
@@ -23,7 +29,13 @@ public class IpAddressService {
         return ipAddressRepository.findById(id);
     }
 
+    @Transactional
     public IpAddress save(IpAddress ipAddress) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        Optional<User> userOpt = userRepository.findByUsername(username);
+        userOpt.ifPresent(user -> ipAddress.setWorker(user.getName()));
+        ipAddress.setUpdatedAt(LocalDateTime.now());
         return ipAddressRepository.save(ipAddress);
     }
 
